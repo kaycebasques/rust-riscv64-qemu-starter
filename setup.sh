18f5eb18604ef96c9fe07a132e0e1b25791d5b59
Kayce Basques <kayce@google.com>
2023-03-06 15:19:43 -0800

    Add logic for building QEMU from source

--- setup.sh
+++ setup.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
+mkdir tools
+cd tools
+
 # Install QEMU.
-sudo apt install qemu qemu-user qemu-system-misc
+# sudo apt install qemu-system-misc
+wget https://download.qemu.org/qemu-7.2.0.tar.xz
+tar xvJf qemu-7.2.0.tar.xz
+cd qemu-7.2.0
+./configure --target-list=riscv64-softmmu
+make
 
 # Download and extract the RISC-V GNU Compiler Toolchain.
 ARCHIVE_DATE="2023.02.25"
 ARCHIVE_FILENAME="riscv64-glibc-ubuntu-22.04-nightly-$ARCHIVE_DATE-nightly.tar.gz"
 ARCHIVE_URL="https://github.com/riscv-collab/riscv-gnu-toolchain/releases/download/$ARCHIVE_DATE/$ARCHIVE_FILENAME"
-mkdir tools
-cd tools
 wget $ARCHIVE_URL
 tar -xf $ARCHIVE_FILENAME
 mv riscv riscv-gnu-toolchain
+
 cd ..
 
 # Set up a disk for QEMU.
